44799f7fb58179a6e63fddf61917eeedea14344c
PhucKhanh <nthtpk7h7i@gmail.com>
2024-08-19 07:44:36 +0700

    ...

--- src/main/java/com/phuckhanh/VideoApp/service/AuthenticationService.java
+++ src/main/java/com/phuckhanh/VideoApp/service/AuthenticationService.java
@@ -241,7 +241,7 @@ public class AuthenticationService {
 
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject(account.getUsername())
-                .issuer("https://VideoApp.phuckhanh.com/")
+                .issuer("https://chatappapi.tpkhanh.com/")
                 .issueTime(new Date())
                 .expirationTime(new Date(
                         Instant.now().plus(VALID_DURATION, ChronoUnit.SECONDS).toEpochMilli()
